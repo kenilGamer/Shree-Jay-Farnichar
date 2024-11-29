8a6c1c2bfeb8a25a0c648d453974ecdf93e41e84
kenil Gamer <kenilk677@gmail.com>
2024-11-29 17:31:38 +0530

    swdsw

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => {
     console.error('Failed to connect to MongoDB:', err.message);
-    console.error('Stack:', err.stack);
   });
 const userSchema = new mongoose.Schema({
   name: String,
